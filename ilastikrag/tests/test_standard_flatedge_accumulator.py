d15392bf690ae5a324fd107b41511f941ee4e2b4
Stuart Berg <bergs@janelia.hhmi.org>
2016-03-14 23:57:26 -0400

    tests: Added test for 'flat' regionradii

--- ilastikrag/tests/test_standard_flatedge_accumulator.py
+++ ilastikrag/tests/test_standard_flatedge_accumulator.py
@@ -63,6 +63,33 @@ class TestStandardFlatEdgeAccumulator(object):
         # ... but quantiles_50 seems to be good.
         assert np.isclose(all_features_df['standard_edge_quantiles_50'], all_features_df['standard_edge_quantiles_50']).all()
         
+    def test_regionradii(self):
+        # Create a volume of flat superpixels, where every slice
+        # is the same (except for the actual sp ids)
+        num_sp_per_slice = 200
+        slice_superpixels = generate_random_voronoi((100,200), num_sp_per_slice)
+        
+        superpixels = np.zeros( shape=((10,) + slice_superpixels.shape), dtype=np.uint32 )
+        for z in range(10):
+            superpixels[z] = slice_superpixels + z*num_sp_per_slice
+        superpixels = vigra.taggedView(superpixels, 'zyx')
+
+        rag_flat = Rag( superpixels, flat_superpixels=True )
+        
+        values = np.random.random(size=(superpixels.shape)).astype(np.float32)
+        values = vigra.taggedView(values, 'zyx')
+        
+        flat_features_df = rag_flat.compute_features(values, ['standard_flatedge_regionradii'], edge_type='flat')
+
+        # Now compute the radii using a normal 'dense' rag
+        rag_dense = Rag( superpixels )
+        dense_features_df = rag_dense.compute_features(values, ['edgeregion_edge_regionradii'])
+
+        # Both methods should be reasonably close.
+        combined_features_df = pd.merge(flat_features_df, dense_features_df, how='left', on=['sp1', 'sp2'])
+        assert np.isclose(combined_features_df['standard_flatedge_regionradii_0'], combined_features_df['edgeregion_edge_regionradii_0'], atol=0.001).all()
+        assert np.isclose(combined_features_df['standard_flatedge_regionradii_1'], combined_features_df['edgeregion_edge_regionradii_1'], atol=0.001).all()
+
 
 if __name__ == "__main__":
     import sys

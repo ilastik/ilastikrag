06424782159ae232f5b51d188d95ec89739b9d11
Stuart Berg <bergs@janelia.hhmi.org>
2016-06-09 13:43:02 -0400

    tests: Added dataframe serialization test

--- ilastikrag/tests/test_util.py
+++ ilastikrag/tests/test_util.py
@@ -1,6 +1,11 @@
-import numpy as np
+import tempfile
+import shutil
 
-from ilastikrag.util import label_vol_mapping
+import numpy as np
+import h5py
+
+from ilastikrag.rag import Rag
+from ilastikrag.util import label_vol_mapping, generate_random_voronoi, dataframe_to_hdf5, dataframe_from_hdf5
 
 def test_label_vol_mapping():
     # 1 2
@@ -25,6 +30,33 @@ def test_label_vol_mapping():
     vol2[( vol2 == 3 ).nonzero()] = 7
 
     assert (label_vol_mapping(vol1, vol2) == [0,7,7,4,5]).all()
+
+def test_features_df_serialization():
+    superpixels = generate_random_voronoi((100,200), 200)
+    rag = Rag( superpixels )
+
+    # For simplicity, just make values identical to superpixels
+    values = superpixels.astype(np.float32)
+
+    feature_names = ['standard_edge_mean', 'standard_edge_minimum', 'standard_edge_maximum']
+    features_df = rag.compute_features(values, feature_names)
+
+    tmpdir = tempfile.mkdtemp()
+    try:
+        with h5py.File( tmpdir + '/' + 'test_dataframe.h5', 'w' ) as f:
+            group = f.create_group('test_dataframe')
+            dataframe_to_hdf5( group, features_df )
+        
+        with h5py.File( tmpdir + '/' + 'test_dataframe.h5', 'r' ) as f:
+            group = f['test_dataframe']
+            readback_features_df = dataframe_from_hdf5( group )
+        
+        assert (readback_features_df.columns.values == features_df.columns.values).all()
+        assert (readback_features_df.values.shape == features_df.values.shape)
+        assert (readback_features_df.values == features_df.values).all()
+
+    finally:
+        shutil.rmtree(tmpdir)
 
 if __name__ == "__main__":
     import sys
